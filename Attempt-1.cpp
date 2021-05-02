1e3444b48700c280ab97055a40aeaa5876c94249
Arnav Menon <63289642+arnavmenon@users.noreply.github.com>
2021-05-02 15:20:37 +0530

    Refactor code, Fix space input error

--- Attempt-1.cpp
+++ Attempt-1.cpp
@@ -1,8 +1,4 @@
-#include<stdio.h>
-#include<iostream>
-#include<string.h>
-#include<stdio.h>
-#include<malloc.h>
+#include<bits/stdc++.h>
 #include<vector>
 
 using namespace std;
@@ -14,41 +10,14 @@ using namespace std;
 #define nStates 100 //Max number of states for the Turning Machine Simulator
 #define nAlphabets 20 //Max number of alphabets that a Turing Machine Tape can read and write, including Blank
 
-int atoz_map[26];
-
-struct TM_transition_rule
-{
-    int q; //State Number
-    char w_s; //Symbol to write on Tape
-    char move; //Move left or right
-};
-
-TM_transition_rule* TM_matrix_ptr[nStates][nAlphabets];
-
-char equivalent_character(char c)
-{
-    //Function to encode ' ' as 'b'
-    return (c == ' ')?'b':c;
-}
-
-void print_TM_string(TM_transition_rule *rule)
-{
-    if(!rule) //when no rule exists
-    {
-        cout<<"     /     ";
-    }
-    else
-    {
-        cout<<" q"<<rule->q<<", "<<equivalent_character(rule->w_s)<<","<<rule->move<<"   ";
-    }
-}
+int atoz_map[26];  // Hash map to store final cipher mapping
 
 char* FileName = new char[nFile];
 char* line = new char[nLine];
 char* Alphabet = new char[nAlphabets];
 char special_char;
 
-enum SymbolType{ number, lparen, rparen, comma, dash, _q, _b, _l, _r, _char, eol };
+enum SymbolType{ number, lparen, rparen, _q, _b, _l, _r, _char, eol };
 FILE *file;
 SymbolType symbol;
 
@@ -61,22 +30,54 @@ int pos, //Position of Marker on TM Tape;
     nSimSteps;  // number of simulation steps
 
 
-char* ErrorCodes[] = { "'q'", "number", "'b' or non-special character","'('", "'l', 'r' or '-'", "')'" };
+char* ErrorCodes[] = { "'q'", "number", "'b' or non-special character","'('", "'l' or 'r'", "')'" };
 
-void SkipBlanks()
+
+struct TM_transition_rule
+{
+    int q; //State Number-REFERRRING TO THE STATE IN O/P OF TRANSITION FUNCTION
+    char w_s; //Symbol to write on Tape
+    char move; //Move left or right
+};
+
+TM_transition_rule* TM_matrix_ptr[nStates][nAlphabets];
+
+
+// UTILITY FUNCTIONS FOR READING TM DESCRIPTION FILE -------------------------------------------------------------------------
+
+char read_blank(char c)
+{
+    //Function to encode ' ' as 'b'
+    return (c == ' ')?'b':c;
+}
+
+void print_TM_String(TM_transition_rule *rule)
+{
+    if(!rule) //when no rule exists
+    {
+        cout<<"     /     ";
+    }
+    else
+    {
+        cout<<" q"<<rule->q<<", "<<read_blank(rule->w_s)<<","<<rule->move<<"   ";
+    }
+}
+
+
+void SkipBlanks()  // Skip blanks while reading transition function file
 {
    while (pos < len && line[ pos ] == ' ')
    {
       ++pos;
    }
-}// SkipBlanks
+}
 
-void Error(int i)
+void Error(int i)  // To print syntax error while reading file 
 {
    for(int j = 0; j < pos-1; j++)
     	cout<<" ";
    cout<<"^\nError "<<i<<": "<<ErrorCodes[i]<<"expected\n";
-}//ErrorMsg, when wrong syntax is parsed
+}
 
 bool BlankLine(string line, int len)
 {
@@ -90,7 +91,7 @@ bool BlankLine(string line, int len)
       }
    }
    return blank;
-}// BlankLine
+}
 
 int GetLine(char* line)         // getLine - prints all Comment Lines, and other descriptive parameters
 {
@@ -144,8 +145,8 @@ void nextSymbol(int ScanNumber = 1)     //ReadCurrentSymbolandAssertType, accord
             {
                 case '(': symbol = lparen;  break;
                 case ')': symbol = rparen;  break;
-                case ',': symbol = comma;   break;
-                case '-': symbol = dash;    break;
+                //case ',': symbol = comma;   break;
+                //case '-': symbol = dash;    break;
                 case 'q': symbol = _q;      break;
                 case 'b': symbol = _b;      ch = ' '; break;
                 case 'l': symbol = _l;      break;
@@ -175,7 +176,10 @@ int CharIndex(char a)       // CharIndex - returns column index for character in
    return j;
 }
 
-void TM_S( int *var )       // TM_S - Utility function to initialise state for transition function
+
+// FUNCTIONS TO INITIALIZE AND SIMULATE TURING MACHINE -------------------------------------------------------------------
+
+void TM_State( int *var )       // Utility function to initialise state for transition function
 {
    if(symbol == _q) 
    {
@@ -191,7 +195,7 @@ void TM_S( int *var )       // TM_S - Utility function to initialise state for t
 }
 
 
-void TM_T()
+void TM_Transition()   // Utility fn to intialize transitions for a given state
 {
     char scan_character, rw_character, direction;
     int nextState;
@@ -206,12 +210,12 @@ void TM_T()
             if(symbol == lparen)
             {
                 nextSymbol();
-                TM_S(&nextState);
+                TM_State(&nextState);
                 if(symbol == _char || symbol == _b)
                 {
                     rw_character = special_char;
                     nextSymbol();
-                    if(symbol == _l || symbol == _r || symbol == dash)
+                    if(symbol == _l || symbol == _r)
                     {
                         direction = special_char;
                         rule_ptr = new TM_transition_rule();
@@ -223,7 +227,7 @@ void TM_T()
                         if(symbol == rparen)
                         {
                             nextSymbol(0);
-                            TM_T();
+                            TM_Transition();
                         }
                         else Error(5);
                     }
@@ -237,17 +241,18 @@ void TM_T()
     }
 }
 
-void TM_P()
+void TM_Init()
 {
-    TM_S(&state);
-    TM_T();
+    TM_State(&state);
+    TM_Transition();
 }
 
-void InputTM()
+void InputTM()  // To read description of TM from text file
 {
     GetLine(line);
     mStates = atoi(line);
     mSymbols = GetLine(Alphabet);
+
     for(int i = 0; i < nStates; i++)
     {
         for(int j = 0; j < nAlphabets; j++)
@@ -265,12 +270,12 @@ void InputTM()
         if(pos < len)
         {
             nextSymbol();
-            TM_P();
+            TM_Init();
         }
     }
 }
 
-void ReportTM()     //ReportTM
+void ReportTM()     // To display description of TM
 {
    cout<<"Turing Machine for :- "<<FileName<<endl;
    cout<<"States: "<<mStates<<" Alphabet: "<<Alphabet<<"  ,"<<mSymbols<<" Symbols"<<endl;
@@ -279,7 +284,7 @@ void ReportTM()     //ReportTM
 
    for(int j = 0; j < mSymbols; j++)
    {
-       cout<<"      "<<equivalent_character(Alphabet[j])<<"    ";
+       cout<<"      "<<read_blank(Alphabet[j])<<"    ";
    }
 
    cout<<endl;
@@ -288,14 +293,14 @@ void ReportTM()     //ReportTM
        cout<<"q"<<i;
        for(int j = 0; j < mSymbols; j++)
        {
-           print_TM_string(TM_matrix_ptr[i][j]);
+           print_TM_String(TM_matrix_ptr[i][j]);
        }
        cout<<endl;
    }
 
 }
 
-void PrintConfiguration(int q, string t, int h)     //Utility function to print configuration
+void PrintConfiguration(int q, string t, int h)     //Utility function to print configuration of TM after each step
 {
    char c;
    int i;
@@ -318,24 +323,24 @@ void PrintConfiguration(int q, string t, int h)     //Utility function to print 
 }
 
 int SimulateTM(string ip_string)
-{
-    if(ip_string[0] == '!')
-        return 0;
-    
+{   
     string tape = ip_string;
     int head, q, n, t;
     TM_transition_rule* rule_ptr;
     long long unsigned int steps;
-    for(int i = tape.length(); i < nLine-1; i++)
+
+    for(int i = tape.length(); i < nLine-1; i++)  // initlaizing tape with blanks
         tape.push_back(' ');
     tape.push_back('\0');
     cout<<endl;
+
     head = 0;
     q = 0;
     t = 0;
     steps = 0L;
     cout<<"The Tape Before Simulation"<<endl;
 	PrintConfiguration(q, tape, head);
+
 	while(q >= 0)
     {
         //PrintConfiguration(q, tape, head);
@@ -344,15 +349,16 @@ int SimulateTM(string ip_string)
         {
             steps++;
             q = rule_ptr->q;
-            tape[head] = rule_ptr->w_s;
-            if(rule_ptr->move == '-')
-                break;
-            head += (tolower(rule_ptr->move) == 'r')?1:-1;
-            if(head < 0 || head >= nLine)
+            tape[head] = rule_ptr->w_s;                     // storing the current step
+
+            head += (tolower(rule_ptr->move) == 'r')?1:-1;  // moving left or right depending on transition rule
+
+            if(head < 0 || head >= nLine)                  // tape overflow
             {
                 cout<<"Illegal reference made by the head for position "<<head<<endl;
                 q = -1;
             }
+
             if(nSimSteps > 0)
             {
                 t++;
@@ -374,41 +380,46 @@ int SimulateTM(string ip_string)
     cout<<"--------------------------------------"<<endl;
 
     string final_result(tape);
+
     int result = 0;
     for(auto x : final_result) 
     {
         if(x == '0')
             result++;
+        else if (x=='1'){
+            result=0;
+        }
+        
     }
     
     return result;
 }
 
-//Main function to encrypt with updated hash_map
+// FUNCTION TO ENCRYPT INPUT STRING WITH GENERATED HASH MAP ----------------------------------------------------------------------------------------
+
 void encrypt_string(string s)
 {
-    string encrypted_string;
+    string encrypted;
     for(auto x : s)
     {
         if(islower(x))
         {
             int shift = atoz_map[x-'a'];
-            encrypted_string.push_back(char('a' + shift));
+            encrypted.push_back(char('a' + shift));
         }
         else if(isupper(x))
         {
             int shift = atoz_map[x-'A'];
-            encrypted_string.push_back(char('A' + shift));
+            encrypted.push_back(char('A' + shift));
         }
         else
-            encrypted_string.push_back(x);
+            encrypted.push_back(x);
     }   
     cout<<"The encrypted string for "<<s<<" is:-"<<endl;
-    cout<<encrypted_string<<endl;
+    cout<<encrypted<<endl;
 
     return;
 }
-
 
 
 //main function
@@ -421,12 +432,15 @@ int main()
     cout<<"Enter Required Shift"<<endl;
     cin>>shift;
 
+
+    // Addition TM, to shift character places for cipher-----------------------------------------------------------------
+
     //Formatting input string like 000100 where the 000 stands for c and 00 stands for 2 places of shift
 
-    string shift_delimiter_for_addition;
-    shift_delimiter_for_addition.push_back('1');
+    string addition_delim;
+    addition_delim.push_back('1');
     for(int i = 1; i <= shift; i++)
-        shift_delimiter_for_addition.push_back('0');
+        addition_delim.push_back('0');
     
     vector<string> TM_addition_inputs;
     for(int i = 0; i < 26; i++)
@@ -434,27 +448,25 @@ int main()
         string s = "";
         for(int j = 0; j < i; j ++)
             s.push_back('0');
-        s += shift_delimiter_for_addition;
+        s += addition_delim;
         TM_addition_inputs.push_back(s);
     }
     
     //opening input file that contains the TM transition table for execution
    	FileName = "AdditionTM_in.txt";
+
     if ( ( file = fopen( FileName, "r" ) ) == NULL )
     {
-      printf( "\nunable to open file '%s'\n", FileName );
+      cout<<"\nunable to open file"<<FileName<<"\n";
     }
     else 
     {
-      //printf( "%s\n", FileName );
       cout<<FileName<<endl;
       cout<<"number of simulation steps [0 = indefinite]? ";
-      //printf( "number of simulation steps [0 = indefinite]? " );
       cin>>nSimSteps;
-      //scanf( "%d", &nSimSteps );
       cout<<nSimSteps<<"\n";
-      //printf( "%d\n", nSimSteps );
 	  cout<<"Establishing TM"<<endl;
+
       InputTM();
       ReportTM();
       cout<<"TM Reported"<<endl;
@@ -463,10 +475,12 @@ int main()
             atoz_map[i] = SimulateTM(TM_addition_inputs[i]);
    }
 
-    string delimiter_for_mod;
-    delimiter_for_mod.push_back('1');
+   // Modulus TM, for calculating mod in case of overflow ----------------------------------------------------------------------
+
+    string mod_delim;
+    mod_delim.push_back('1');
     for(int i = 0; i < 26; i++)
-        delimiter_for_mod.push_back('0');
+        mod_delim.push_back('0');
     
     vector<string> TM_mod_inputs;
     for(int i = 0; i < 26; i++)
@@ -474,22 +488,24 @@ int main()
         string s = " ";
         for(int j = 0; j < atoz_map[i]; j ++)
             s.push_back('0');
-        s += delimiter_for_mod;
+        s += mod_delim;
         TM_mod_inputs.push_back(s);
     }
     
     FileName = "AmodBTM_in.txt";
+
     if ( ( file = fopen( FileName, "r" ) ) == NULL )
     {
-      printf( "\nunable to open file '%s'\n", FileName );
+      cout<<"\nunable to open file "<<FileName<<"\n";
     }
     else 
     {
-      printf( "%s\n", FileName );
-      printf( "number of simulation steps [0 = indefinite]? " );
-      scanf( "%d", &nSimSteps );
-      printf( "%d\n", nSimSteps );
+      cout<<FileName<<endl;
+      cout<<"number of simulation steps [0 = indefinite]? ";
+      cin>>nSimSteps;
+      cout<<nSimSteps<<"\n";
 	  cout<<"Establishing TM"<<endl;
+
       InputTM();
       ReportTM();
       cout<<"TM Reported"<<endl;
@@ -501,14 +517,19 @@ int main()
     cout<<"Processing has been done"<<endl<<"-------------------------------"<<endl;
 	cout<<"The given shift was "<<shift<<endl;
     cout<<shift<<endl;
+
+    // Printing encrypted string ---------------------------------------------------------------------------------------------
 	
 	cout<<"Encrypted Hash Map"<<endl;
     for(int i = 0; i < 26; i++)
         cout<<atoz_map[i]<<endl;
    
+    cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n'); // flushing
     string ip_string;
-    cout<<"Enter String to Encrypt"<<endl;
-    cin>>ip_string;
-    encrypt_string(ip_string);
+    cout<<"Enter String to Encrypt: ";
+    getline(cin,ip_string);
+
+    encrypt_string(string(ip_string));      // Display final string
+
     return 0;
 }// _tmain
